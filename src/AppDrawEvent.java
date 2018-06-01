0fa0894e4dd01c2874084a16a7d8ec94a1a43d85
BuildTools <unconfigured@null.spigotmc.org>
2018-06-01 14:01:49 +0200

    Everything is working

--- src/AppDrawEvent.java
+++ src/AppDrawEvent.java
@@ -25,7 +25,7 @@ class AppDrawPanel extends JPanel {
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawLine(0,0,0,0);
+        g.drawLine(0, 0, 0, 0);
     }
 }
 
@@ -38,10 +38,12 @@ class ExitAction extends MouseAdapter {
 class Original extends MouseAdapter {
 
     public BufferedImage img, pic;
+    JLabel picLabel;
 
-    Original(BufferedImage img, BufferedImage pic){
-        this.img=img;
-        this.pic=pic;
+    Original(BufferedImage img, BufferedImage pic, JLabel picLabel) {
+        this.img = img;
+        this.pic = pic;
+        this.picLabel = picLabel;
     }
 
     public void mouseClicked(MouseEvent e) {
@@ -53,30 +55,24 @@ class Original extends MouseAdapter {
             for (int j = 0; j < width; j++) {
                 int p = img.getRGB(j, i);
 
-                pic.setRGB(2*j,2*i,p);
-                pic.setRGB(2*j,2*i+1,p);
-                pic.setRGB(2*j+1, 2*i,p);
-                pic.setRGB(2*j+1, 2*i+1, p);
+                pic.setRGB(2 * j, 2 * i, p);
+                pic.setRGB(2 * j, 2 * i + 1, p);
+                pic.setRGB(2 * j + 1, 2 * i, p);
+                pic.setRGB(2 * j + 1, 2 * i + 1, p);
             }
         }
-        //TODO: variablen namen immer klein
-        JFrame O = new JFrame("Original");
-        JLabel test = new JLabel(new ImageIcon(pic));
-        O.add(test);
-        O.pack();
-        O.setVisible(true);
+        picLabel.setIcon(new ImageIcon(pic));
     }
 }
 
-//TODO: Klassen immer groß schreiben
-class grayScale extends MouseAdapter {
+class GrayScale extends MouseAdapter {
 
     BufferedImage img, pic;
     JLabel picLabel;
 
-    grayScale(BufferedImage img, BufferedImage pic, JLabel picLabel){
-        this.img=img;
-        this.pic=pic;
+    GrayScale(BufferedImage img, BufferedImage pic, JLabel picLabel) {
+        this.img = img;
+        this.pic = pic;
         this.picLabel = picLabel;
     }
 
@@ -98,30 +94,114 @@ class grayScale extends MouseAdapter {
                 //get blue
                 int b = p & 0xff;
 
-                int mix =(r+g+b)/3;
+                int mix = (r + g + b) / 3;
 
-                p =  (mix << 16) | (mix << 8) | mix;
+                p = (mix << 16) | (mix << 8) | mix;
 
                 //set the pixel value
-                pic.setRGB(2*j, 2*i, p);
-                pic.setRGB(2*j,2*i+1,p);
-                pic.setRGB(2*j+1, 2*i,p);
-                pic.setRGB(2*j+1, 2*i+1, p);
+                pic.setRGB(2 * j, 2 * i, p);
+                pic.setRGB(2 * j, 2 * i + 1, p);
+                pic.setRGB(2 * j + 1, 2 * i, p);
+                pic.setRGB(2 * j + 1, 2 * i + 1, p);
             }
         }
-        //NEU
         picLabel.setIcon(new ImageIcon(pic));
-        //das war das was du wolltest oder? Die größe stimmt nicht
-
-        //TODO: variablen namen immer klein
-        JFrame O = new JFrame("Grayscale");
-        JLabel test = new JLabel(new ImageIcon(pic));
-        O.add(test);
-        O.pack();
-        O.setVisible(true);
     }
 
 }
+
+class Pattern extends MouseAdapter {
+
+    BufferedImage img, pic;
+    JLabel picLabel;
+
+    Pattern(BufferedImage img, BufferedImage pic, JLabel picLabel) {
+        this.img = img;
+        this.pic = pic;
+        this.picLabel = picLabel;
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        int width = img.getWidth();
+        int height = img.getHeight();
+
+        //highest rgb
+        int v = 0;
+        //get pixel value
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int p = img.getRGB(j, i);
+
+                //get red
+                int r = (p >> 16) & 0xff;
+                if (r > v) v = r;
+                //get green
+                int g = (p >> 8) & 0xff;
+                if (g > v) v = g;
+
+                //get blue
+                int b = p & 0xff;
+                if (b > v) v = b;
+            }
+        }
+        v/=5;
+        int r = 255;
+        int g = 255;
+        int b = 255;
+        int wh = (r << 16) | (g << 8) | b;
+        r = 0;
+        g = 0;
+        b = 0;
+        int bl = (r << 16) | (g << 8) | b;
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int p = img.getRGB(j, i);
+
+                //get red
+                r = (p >> 16) & 0xff;
+                //get green
+                g = (p >> 8) & 0xff;
+
+                //get blue
+                b = p & 0xff;
+
+                int mix = (r + g + b) / 3;
+
+                //set the pixel value
+                if (mix > (4 * v)) {
+                    pic.setRGB(2 * j, 2 * i, wh);
+                    pic.setRGB(2 * j, 2 * i + 1, wh);
+                    pic.setRGB(2 * j + 1, 2 * i, wh);
+                    pic.setRGB(2 * j + 1, 2 * i + 1, wh);
+                } else if (mix > (3 * v)) {
+                    pic.setRGB(2 * j, 2 * i, bl);
+                    pic.setRGB(2 * j, 2 * i + 1, wh);
+                    pic.setRGB(2 * j + 1, 2 * i, wh);
+                    pic.setRGB(2 * j + 1, 2 * i + 1, wh);
+                } else if (mix > (2 * v)) {
+                    pic.setRGB(2 * j, 2 * i, bl);
+                    pic.setRGB(2 * j, 2 * i + 1, wh);
+                    pic.setRGB(2 * j + 1, 2 * i, wh);
+                    pic.setRGB(2 * j + 1, 2 * i + 1, bl);
+                } else if (mix > v) {
+                    pic.setRGB(2 * j, 2 * i, wh);
+                    pic.setRGB(2 * j, 2 * i + 1, bl);
+                    pic.setRGB(2 * j + 1, 2 * i, bl);
+                    pic.setRGB(2 * j + 1, 2 * i + 1, bl);
+                } else {
+                    pic.setRGB(2 * j, 2 * i, bl);
+                    pic.setRGB(2 * j, 2 * i + 1, bl);
+                    pic.setRGB(2 * j + 1, 2 * i, bl);
+                    pic.setRGB(2 * j + 1, 2 * i + 1, bl);
+                }
+            }
+        }
+        picLabel.setIcon(new ImageIcon(pic));
+    }
+
+}
+
 
 public class AppDrawEvent {
 
@@ -139,14 +219,14 @@ public class AppDrawEvent {
         }
 
         ImageIcon icon = new ImageIcon(img);
-        icon.setImage(icon.getImage().getScaledInstance(icon.getIconWidth()*2, icon.getIconHeight()*2, Image.SCALE_DEFAULT));
+        icon.setImage(icon.getImage().getScaledInstance(icon.getIconWidth() * 2, icon.getIconHeight() * 2, Image.SCALE_DEFAULT));
         BufferedImage bi = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_RGB);
         Graphics g = bi.createGraphics();
         // paint the Icon to the BufferedImage.
-        icon.paintIcon(null, g, 0,0);
+        icon.paintIcon(null, g, 0, 0);
         g.dispose();
 
-        JLabel picLabel = new JLabel(new ImageIcon(img));
+        JLabel picLabel = new JLabel(new ImageIcon(bi));
 
         JFrame frame = new AppFrame("APP - Übung 5");
         JPanel butpanel = new JPanel(new BorderLayout());
@@ -176,11 +256,14 @@ public class AppDrawEvent {
         ExitAction m = new ExitAction();
         exit.addMouseListener(m);
 
-        Original n = new Original(img, bi);
+        Original n = new Original(img, bi, picLabel);
         oButton.addMouseListener(n);
 
-        grayScale l = new grayScale(img, bi, picLabel);
+        GrayScale l = new GrayScale(img, bi, picLabel);
         gsButton.addMouseListener(l);
+
+        Pattern p = new Pattern(img, bi, picLabel);
+        pButton.addMouseListener(p);
 
         frame.pack();
         frame.setVisible(true);
