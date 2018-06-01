007ed911e94be34122b49bc1be34e0db1acc8e3c
BuildTools <unconfigured@null.spigotmc.org>
2018-06-01 12:39:29 +0200

    Progress

--- src/AppDrawEvent.java
+++ src/AppDrawEvent.java
@@ -19,10 +19,18 @@ class AppDrawPanel extends JPanel {
         return new Dimension(500, 200);
     }
 
+    int x1, x2, y1, y2;
+
+    void setxy(int x1, int x2, int y1, int y2){
+        this.x1=x1;
+        this.x2=x2;
+        this.y1=y1;
+        this.y2=y2;
+    }
+
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawLine(10, 10, 490, 190);
-        g.drawString("Ein einfaches Panel", 50, 100);
+        g.drawLine(x1, y1, x2, y2);
     }
 }
 
@@ -33,20 +41,44 @@ class ExitAction extends MouseAdapter {
 }
 
 class Original extends MouseAdapter {
-    public void mouseClicked(MouseEvent e) {
+    public BufferedImage img, pic;
 
+    Original(BufferedImage img, BufferedImage pic){
+        this.img=img;
+        this.pic=pic;
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        int width = img.getWidth();
+        int height = img.getHeight();
+
+        //get pixel value
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int p = img.getRGB(j, i);
+
+                pic.setRGB(j,i,p);
+                pic.setRGB(j,i+1,p);
+                pic.setRGB(j+1, i,p);
+                pic.setRGB(j+1, i+1, p);
+            }
+        }
+        JFrame O = new JFrame("Original");
+        JLabel test = new JLabel(new ImageIcon(pic));
+        O.add(test);
+        O.pack();
+        O.setVisible(true);
     }
 }
 
 class grayScale extends MouseAdapter {
     public BufferedImage img;
     String path;
-    JLabel pic;
+    JPanel pic;
 
     grayScale(BufferedImage img, String path, JLabel pic){
         this.img=img;
         this.path=path;
-        this.pic=pic;
     }
 
     public void mouseClicked(MouseEvent e) {
@@ -75,21 +107,30 @@ class grayScale extends MouseAdapter {
                 img.setRGB(j, i, p);
             }
         }
-        pic = new JLabel(new ImageIcon(img));
+        JFrame O = new JFrame("Grayscale");
+        JLabel test = new JLabel(new ImageIcon(img));
+        O.add(test);
+        O.pack();
+        O.setVisible(true);
     }
 }
 
 public class AppDrawEvent {
-    public static BufferedImage img;
-
     public static void main(String[] args) {
 
         String path = args[0];
 
         try {
-            img = ImageIO.read(new File(path));
-            JLabel picLabel = new JLabel(new ImageIcon(img));
+            BufferedImage img = ImageIO.read(new File(path));
+            ImageIcon icon = new ImageIcon(img);
+            icon.setImage(icon.getImage().getScaledInstance(icon.getIconWidth()*2, icon.getIconHeight()*2, Image.SCALE_DEFAULT));
+            BufferedImage bi = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_RGB);
+            Graphics g = bi.createGraphics();
+            // paint the Icon to the BufferedImage.
+            icon.paintIcon(null, g, 0,0);
+            g.dispose();
 
+            JLabel picLabel = new JLabel(new ImageIcon(img));
 
             JFrame frame = new AppFrame("APP - Übung 5");
             JPanel butpanel = new JPanel(new BorderLayout());
@@ -119,12 +160,11 @@ public class AppDrawEvent {
             ExitAction m = new ExitAction();
             exit.addMouseListener(m);
 
-            Original n = new Original();
+            Original n = new Original(img, bi);
             oButton.addMouseListener(n);
 
             grayScale l = new grayScale(img, path, picLabel);
             gsButton.addMouseListener(l);
-            picLabel=l.pic;
 
 
             frame.pack();
